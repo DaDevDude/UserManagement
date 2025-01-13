07eabcb38c4f6883b21eb434bb58be4e45d3a18f
SahilPatil <sahilpatil@onelabventures.com>
2025-01-13 16:54:27 +0530

    updated frontend api in cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://main.d2dwjnixvn5b5o.amplifyapp.com",
     methods: ["GET", "POST", "PATCH", "DELETE"],
   })
 );
